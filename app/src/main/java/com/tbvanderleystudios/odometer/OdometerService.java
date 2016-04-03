8a9a6aa6739f4a9b7bd897b6d0f102d7b9c6fa52
TBVanderley4545 <TBVanderley@gmail.com>
2016-04-03 14:19:55 -0400

    | Create inner class in OdometerService to extend Binder |

--- app/src/main/java/com/tbvanderleystudios/odometer/OdometerService.java
+++ app/src/main/java/com/tbvanderleystudios/odometer/OdometerService.java
@@ -2,14 +2,21 @@ package com.tbvanderleystudios.odometer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class OdometerService extends Service {
-    public OdometerService() {
+
+    private final IBinder mBinder = new OdometerBinder();
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
