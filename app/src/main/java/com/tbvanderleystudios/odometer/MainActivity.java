5ff61fa11c7144ef4c900149ac7c9ea30e30c9fb
tylervanderley <tbvanderley@gmail.com>
2016-04-03 21:56:34 -0400

    | Overrode onCreate() in OdometerService class. | | Added a LocationListener in the OdometerService class's overridden onCreate() | | Added a LocationManager in onCreate() | | Added onDestroy() | | Checked to ensure the user granted ACCESS_FINE_LOCATION permission | | Updated activity_main.xml | | Created a ServiceConnector object in MainActivity.java | | Overrode onStart() in MainActivity.java | | Overrode onStop() in MainActivity.java | | Added watchMileage() to MainActivity.java |

--- app/src/main/java/com/tbvanderleystudios/odometer/MainActivity.java
+++ app/src/main/java/com/tbvanderleystudios/odometer/MainActivity.java
@@ -1,13 +1,74 @@
 package com.tbvanderleystudios.odometer;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private OdometerService mOdometerService;
+    private boolean mBound = false;
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) service;
+            mOdometerService = odometerBinder.getOdometer();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        watchMileage();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+    }
+
+    private void watchMileage() {
+        final TextView distanceTextView = (TextView) findViewById(R.id.distanceTextView);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if(mOdometerService != null) {
+                    distance = mOdometerService.getMiles();
+                }
+                String distanceString = String.format("%1$,.2f MILES", distance);
+                if (distanceTextView != null) {
+                    distanceTextView.setText(distanceString);
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
--- app/src/main/java/com/tbvanderleystudios/odometer/OdometerService.java
+++ app/src/main/java/com/tbvanderleystudios/odometer/OdometerService.java
@@ -1,13 +1,25 @@
 package com.tbvanderleystudios.odometer;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 
 public class OdometerService extends Service {
 
     private final IBinder mBinder = new OdometerBinder();
+    private static double mDistanceInMeters;
+    private static Location mLastLocation = null;
+    private LocationManager mLocationManager;
+    private LocationListener mListener;
 
     public class OdometerBinder extends Binder {
         OdometerService getOdometer() {
@@ -16,7 +28,64 @@ public class OdometerService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        mListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (mLastLocation == null) {
+                    mLastLocation = location;
+                }
+
+                mDistanceInMeters += location.distanceTo(mLastLocation);
+
+                mLastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000,
+                    1,
+                    mListener);
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mLocationManager != null && mListener != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                mLocationManager.removeUpdates(mListener);
+                mLocationManager = null;
+                mListener = null;
+            }
+
+        }
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
         return mBinder;
+    }
+
+    public double getMiles() {
+        return mDistanceInMeters / 1609.344;
     }
 }
